72d5f0d95e4f05f94b9c47428031b9ee1cff4771
ILIASSTAJ <iliassbasrtaj@gmail.com.com>
2023-03-22 13:14:19 -0700

    tt

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print alphabets 10 times 
+ * let's do it 
+ * */
+
+void print_alphabet_x10(void) 
+{
+	int i, co;
+
+	co = 10;
+
+	while ( co < 10)
+	{
+		for( i='a' ; i<= 'z' ; i++ )
+		{ 
+		       _putchar(i);
+                }
+		co++;
+
+	}
+}
+
+
+
+"
+

0f1ba71e33c30bc5124cbaf6e1ae11b12beb92f0
ILIASSTAJ <iliassbasrtaj@gmail.com.com>
2023-03-24 05:22:54 -0700

    ff

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,9 @@
-#ifndef MAIN_H
+#ifndef MAIN_N
 #define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
-carrie@ubuntu:/debugging$ 
-int largest_number(int a, int b, int c)
-print_remaining_days()

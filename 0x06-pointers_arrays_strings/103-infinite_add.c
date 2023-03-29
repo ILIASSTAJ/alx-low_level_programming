9cfb83842efa9e7135df53974b249c124879a37d
ILIASSTAJ <iliassbasrtaj@gmail.com.com>
2023-03-29 14:23:32 -0700

    fix things

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,46 +1,89 @@
 #include "main.h"
+#include <stdio.h>
 /**
-  * infinite_add - adds two numbers
-  * @n1: number1
-  * @n2: number2
-  * @r: result
-  * @size_r: size result
-  * Return: r addition
-**/
+ * infinite_add - adds two integers stored as strings
+ *
+ * @n1: first integer string to add
+ * @n2: second integer string to add
+ * @r: array to store resulting string in
+ * @size_r: size of array r
+ *
+ * Return: the summed string in r. If r is too small for the result,
+ * return 0;
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, a, b, c, n, aux, dec = 0;
+	int carry = 0, index = 0, index2;
+	char *s1 = n1, *s2 = n2;
 
-	while (n1[i] != '\0')
+	while (*s1 != 0)
+		s1++;
+	while (*s2 != 0)
+		s2++;
+	size_r--;
+	r[size_r] = 0;
+	s1--;
+	s2--;
+	while (s2 != n2 - 1 && s1 != n1 - 1)
 	{
-	i++;
+		r[index] = *s2 - '0' + *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry++;
+			r[index] -= 10;
+		}
+		index++;
+		s2--;
+		s1--;
+		if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || carry == 1))
+			return (0);
 	}
-	while (n2[j] != '\0')
+	while (s1 != n1 - 1)
 	{
-	j++;
+		r[index] = *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s1--;
+		index++;
+		if (size_r == index && (s1 != n1 - 1 ||  carry == 1))
+			return (0);
 	}
-	for (n = 0; n < j || n < i; n++)
+	while (s2 != n2 - 1)
 	{
-	a = (i - n) > 0 ? (n1[i - n - 1] - '0') : 0;
-	b = (j - n) > 0 ? (n2[j - n - 1] - '0') : 0;
-	c = a + b + dec;
-	r[n] = (c % 10) + '0';
-	dec = c > 9 ? 1 : 0;
+		r[index] = *s2 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s2--;
+		index++;
+		if (size_r == index && (s2 != n2 - 1 || carry == 1))
+			return (0);
 	}
-	if (dec == 1)
-	{ r[n] = '1';
-	r[n + 1] = '\0'; }
+	if (carry == 1)
+	{
+		r[index] = '1';
+		r[index + 1] = 0;
+	}
 	else
-	{ r[n] = '\0';
-	n--; }
-	for (i = 0; i < n + 1; i++)
 	{
-	for (j = 0; j < (n - i); j++)
+		r[index--] = 0;
+	}
+	index2 = 0;
+	while (index2 <= index)
 	{
-	aux = r[j + 1];
-	r[j + 1] = r[j];
-	r[j] = aux;
+		carry = r[index];
+		r[index] = r[index2];
+		r[index2] = carry;
+		index--;
+		index2++;
 	}
-	}
-return (n < size_r - 1 ? r : 0);
+	return (r);
 }

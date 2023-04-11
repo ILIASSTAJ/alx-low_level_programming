1ce53b1b6eef15df688140a1626f0043efead824
ILIASSTAJ <iliassbasrtaj@gmail.com.com>
2023-04-11 07:25:47 -0700

    advanced tasks

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,48 +1,54 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-/**
-* argstostr - concatenates all the arguments of your program
-* @ac: size
-* @av: array arguments
-* Return: double pointer
-*/
+#include <stdio.h>
 
+/**
+  * argstostr - convert the params passed to the program to string
+  * @ac: the argument count
+  * @av: the argument vector
+  *
+  * Return: ...
+  */
 char *argstostr(int ac, char **av)
 {
-	int i = 0, k = 0, count = 0;
+	int ch = 0, i = 0, j = 0, k = 0;
 	char *s;
 
 	if (ac == 0 || av == NULL)
-	return (0);
+		return (NULL);
+
 	while (i < ac)
 	{
-	k = 0;
-	while (av[i][k] != '\0')
-	{
-		count++;
-		k++;
-	}
-	i++;
-	}
-	s = malloc((count + ac + 1) * sizeof(char));
-	if (s == '\0')
-	return (0);
-	i = 0;
-	count = 0;
-	while (i < ac)
-	{
-		k = 0;
-		while (av[i][k] != '\0')
+		while (av[i][j])
 		{
-			s[count] = av[i][k];
-			k++;
-			count++;
+			ch++;
+			j++;
 		}
-		s[count] = 10;
-		count++;
+
+		j = 0;
+		i++;
+	}
+
+	s = malloc((sizeof(char) * ch) + ac + 1);
+
+	i = 0;
+	while (av[i])
+	{
+		while (av[i][j])
+		{
+			s[k] = av[i][j];
+			k++;
+			j++;
+		}
+
+		s[k] = '\n';
+
+		j = 0;
 		k++;
 		i++;
 	}
-return (s);
+
+	k++;
+	s[k] = '\0';
+	return (s);
 }

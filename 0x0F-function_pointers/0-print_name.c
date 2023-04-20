5ba993764e76d6ee2c635ab7ce65a22b7420190b
ILIASSTAJ <iliassbasrtaj@gmail.com.com>
2023-04-19 20:00:07 -0700

    all tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
-include "function_pointers.h"
+#include "function_pointers.h"
 /**
- * print_name - print the name
- * @name: name to print
- * @f: function to use
- */
+* print_name - prints a name
+* @name: name of the person
+* @f: funtion that prints
+* Return: Nothing.
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+if (f)
+f(name);
 }

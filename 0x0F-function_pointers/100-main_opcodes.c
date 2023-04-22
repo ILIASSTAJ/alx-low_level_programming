1dd0d502dc6479f6d72069d252920fe2d1ee7051
ILIASSTAJ <iliassbasrtaj@gmail.com.com>
2023-04-22 10:45:57 -0700

    advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <udis86.h>
+
 /**
-* main -  prints the opcodes of its own main function.
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0
-*/
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	ud_t ud_obj;
+	int val = 0, i = 0;
+
+	if (argc == 2)
 	{
-		printf("Error\n");
-		exit(1);
+		val = atoi(argv[1]);
+
+		if (val < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+
+		ud_unit(&ud_obj);
+		ud_set_input_buffer(&ud_obj, argv[1], val);
+		ud_set_mode(&ud_obj, 64);
+		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+		while (ud_disassemble(&ud_obj))
+		{
+			printf("\t%s\n", ud_insn_hex(&ud_obj));
+		}
 	}
-	if (atoi(argv[1]) < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-return (0);
+
+	return (0);
 }
